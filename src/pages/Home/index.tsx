92ec7d53ddc4e4c1dddbe2342469b10ada65d005
EuNetu <euneto2501@gmail.com>
2025-05-04 04:51:27 -0300

    feat: implementa validação no form da página Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,39 @@
 import { Play } from "phosphor-react";
 import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+    task: zod.string().min(1, "Informe a tarefa"),
+    minutesAmount: zod
+        .number()
+        .min(5)
+        .max(60),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
 export function Home() {
+    const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+        resolver: zodResolver(newCycleFormValidationSchema),
+        defaultValues: {
+            task: '',
+            minutesAmount: 0,
+        }
+    });
+
+    function handleCreateNewCycle(data: any) {
+        console.log(data);
+        reset();
+    }
+
+    const task = watch("task");
+    const isSubmitDisabled = !task;
+
     return (
         <HomeContainer>
-            <form>
+            <form onSubmit={handleSubmit(handleCreateNewCycle)}>
                 <FormContainer>
 
                     <label htmlFor="task">Vou trabalhar em</label>
@@ -12,6 +41,7 @@ export function Home() {
                         id="task" 
                         list="task-suggestions" 
                         placeholder="Dê um nome para o seu projeto" 
+                        {...register("task")}
                     />
 
                     <datalist id="task-suggestions">
@@ -27,6 +57,7 @@ export function Home() {
                         id="minutesAmount" 
                         placeholder="00" 
                         step={5} min={5} max={60} 
+                        {...register("minutesAmount", { valueAsNumber: true })}
                     />
 
                     <span>minutos.</span>
@@ -40,7 +71,7 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <StartCountdownButton type="submit">
+                <StartCountdownButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24} />
                     Começar
                 </StartCountdownButton>
