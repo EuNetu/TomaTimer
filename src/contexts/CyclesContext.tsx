55ea533169a430bf73ffd2536607f3417b079b6e
EuNetu <euneto2501@gmail.com>
2025-05-12 01:43:22 -0300

    feat: implementa armazenamento de dados no localStorage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,6 +1,7 @@
-import { createContext, ReactNode, useReducer, useState } from 'react'
+import { createContext, ReactNode, use, useEffect, useReducer, useState } from 'react'
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer'
 import { addNewCycleAction, interruptCurrentCycleAction, markCurrentCycleAsFinishedAction } from '../reducers/cycles/actions'
+import { differenceInSeconds } from 'date-fns'
 
 interface CreateCycleData {
     task: string
@@ -33,10 +34,30 @@ export function CyclesContextProvider({
         cyclesReducer, {
         cycles: [],
         activeCycleId: null,
+    }, ( initialState ) => {
+        const storedStateAsJSON = localStorage.getItem(
+            '@TomaTimer:cycles-state-1.0.0'
+        )
+        if (storedStateAsJSON) {
+            return JSON.parse(storedStateAsJSON)
+        }
+        return initialState
     })
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+    const [amountSecondsPassed, setAmountSecondsPassed] = useState(()   => {
+        if (cyclesState.activeCycleId) {
+            return differenceInSeconds(
+                new Date(),
+                new Date(cyclesState.activeCycleId),
+            )
+        }
+        return 0
+    })
 
+    useEffect(() => {
+        const stateJSON = JSON.stringify(cyclesState)
+        localStorage.setItem('@TomaTimer:cycles-state-1.0.0', stateJSON)
+    }, [cyclesState])
 
     const { cycles, activeCycleId } = cyclesState
 
